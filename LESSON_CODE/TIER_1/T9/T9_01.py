b9ce23ab026f71ca2d4d27f095c4f7dbbbe74dcc
ArturLaski <162509395+ArturLaski@users.noreply.github.com>
2024-03-12 17:23:13 +0100

    Delete LESSON_CODE/TIER_1/T9/T9_01.py

--- LESSON_CODE/TIER_1/T9/T9_01.py
+++
@@ -1,8 +0,0 @@
-from functools import reduce
-
-def sum_numbers(numbers):
-    return reduce(lambda x, y: x + y, numbers, 0)
-
-# Test the function
-numbers = [3, 4, 6, 9, 34, 12]
-print(sum_numbers(numbers))
