c31a30c5c1200a866f63d874920d93717bd51003
ArturLaski <162509395+ArturLaski@users.noreply.github.com>
2024-03-18 11:02:07 +0100

    Create T11_01.py

---
+++ LESSON_CODE/TIER_1/T11/T11_01.py
@@ -0,0 +1,11 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+# Example usage
+point = Point(5, 10)
+print(point.x)  # Output: 5
+print(point.y)  # Output: 10
+
+        
